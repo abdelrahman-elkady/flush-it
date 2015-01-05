c600583e8ab624fec531c1c66fd5005077d67f58
kady <elkadyabdelrahman@gmail.com>
2015-01-05 22:24:50 +0200

    Filtering non-uninstallable apps from the list

--- app/src/main/java/activities/SettingsActivity.java
+++ app/src/main/java/activities/SettingsActivity.java
@@ -2,6 +2,9 @@ package activities;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -13,6 +16,7 @@ import android.widget.ListView;
 import com.hideme.hideme.R;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import adapters.ApplicationsAdapter;
 
@@ -35,16 +39,30 @@ public class SettingsActivity extends ActionBarActivity {
 
         mAppsListView = (ListView) findViewById(R.id.settings_lst_application_list);
 
-        final Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
-        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        final Intent appQueryIntent = new Intent(Intent.ACTION_MAIN, null);
+        appQueryIntent.addCategory(Intent.CATEGORY_LAUNCHER);
 
         ArrayList<ResolveInfo> dataArrayList = new ArrayList<>();
-        dataArrayList.addAll(getPackageManager().queryIntentActivities(mainIntent, 0));
+        dataArrayList.addAll(getPackageManager().queryIntentActivities(appQueryIntent, PackageManager.GET_META_DATA));
+
+
+        // Filtering : Removing uninstallable apps !
+        Iterator<ResolveInfo> iterator = dataArrayList.iterator();
+        while (iterator.hasNext()) {
+            ResolveInfo info = iterator.next();
+            if (isSystemPackage(info))
+                iterator.remove();
+        }
+
 
         Log.d("INSTALLED_APPS",dataArrayList.toString());
         mAdapter = new ApplicationsAdapter(this,dataArrayList);
         mAppsListView.setAdapter(mAdapter);
 
 
+    }
+
+    private boolean isSystemPackage(ResolveInfo info) {
+        return (info.activityInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0;
     }
 }
