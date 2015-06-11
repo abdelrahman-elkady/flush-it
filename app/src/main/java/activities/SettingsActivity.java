83305ff8a4d2c13f7f4b5c1cba3fd3cde8b91989
kady <elkadyabdelrahman@gmail.com>
2015-06-12 01:59:55 +0200

    Using Activity instead of ActionBarActivity because min API is 15

--- app/src/main/java/activities/SettingsActivity.java
+++ app/src/main/java/activities/SettingsActivity.java
@@ -1,5 +1,6 @@
 package activities;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
@@ -24,7 +25,7 @@ import butterknife.InjectView;
  *
  * @author kady
  */
-public class SettingsActivity extends ActionBarActivity {
+public class SettingsActivity extends Activity {
 
     @InjectView(R.id.settings_lst_application_list)
     ListView mAppsListView;
